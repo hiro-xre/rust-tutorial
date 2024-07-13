58260e2d3f0cad45d258a0871446c16bf63241cc
hiroto-hara <xre0605@gmail.com>
2024-07-13 21:53:59 +0900

    メソッドの実装

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+}
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 fn main() {
@@ -8,14 +14,16 @@ fn main() {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
 
-    println!(
-        // 長方形の面積は、{}平方ピクセルです
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    // rect1にrect2ははまり込む？
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
