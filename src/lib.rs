38c66b74413f26aa33f0b408f0811b85efbd03ad
hiroto-hara <xre0605@gmail.com>
2024-09-09 21:14:51 +0900

    モジュールツリー

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,27 @@ mod front_of_house {
     }
 }
 
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
 pub fn eat_at_restaurant() {
     crate::front_of_house::hosting::add_to_waitlist();
-
     front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
 }
